c0e605ed82292b3c5d9b678cb03ba1b5265df2e5
lcxintern <mothapol@lcx.co.za>
2020-02-12 11:34:01 +0200

    added changes

---
+++ Pages/Home_page.php
@@ -0,0 +1,18 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home page</title>
+</head>
+<body>
+
+<a href="Make_Bookings.php">Book a slot</a><br>
+<a href="View_Schedule.php">What's happening</a>
+
+</body>
+</html>
+
---
+++ Pages/View_Schedule.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home page</title>
+</head>
+<body>
+
+<a href="Home_page.php">Home</a><br>
+
+</body>
+</html>
